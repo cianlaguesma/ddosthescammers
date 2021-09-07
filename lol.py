e5532dc7c86ab5ec0473ce994a6b0a0f6b91b35c
Sean Pe <sean.m.s.pe@gmail.com>
2021-09-08 02:21:03 +0800

    better random word generation

--- lol.py
+++ lol.py
@@ -4,8 +4,7 @@ import sys
 from faker import Faker
 url = 'https://dappsauthes.com/process.php'
 
-fake = Faker()
-
+faker = Faker()
 
 
 threads = []
@@ -17,19 +16,20 @@ def do_thread():
             'recovery-phrase': 'simple scam amazing administrator twelve words secret recovery phase update restart notes',
             'submit': 'submit'
         }
-        data['recovery-phrase'] = "{} {} {} {}".format(fake.bs(),fake.bs(),fake.bs(),fake.bs()) 
+        fakeWords = faker.words(12)
+        listToStr = ' '.join([str(elem) for elem in fakeWords])
+        data['recovery-phrase'] = listToStr
         # print(data['recovery-phrase'])
         response  = requests.post(url, data)
         print(response)
-        sys.stdout.flush()
-do_thread()
-# for x in range (0,10):
-#     t = threading.Thread(target=do_thread)
-#     t.daemon = True
-#     threads.append(t)
+# do_thread()
+for x in range (0,10):
+    t = threading.Thread(target=do_thread)
+    t.daemon = True
+    threads.append(t)
 
-# for x in range(0,10):
-#     threads[x].start()
+for x in range(0,10):
+    threads[x].start()
 
-# for x in range(0,10):
-#     threads[x].join()
+for x in range(0,10):
+    threads[x].join()
